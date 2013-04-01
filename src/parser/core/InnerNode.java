1c79a9bee7df48cb30aed0be627a26afe689ab11
KircheisHe <hechenzhang@126.com>
2013-04-01 18:41:43 +0800

    InnerNode is added.

---
+++ src/parser/core/InnerNode.java
@@ -0,0 +1,51 @@
+package parser.core;
+
+import java.lang.reflect.Method;
+
+/*
+ * InnerNode is one basic kind of node in the DAG.
+ * It is not the input nor the output.
+ * So, InnerNode will need a executor to do the processing and a distributor to distribute
+ * the data.
+ * 
+ * check() is used to check whether the corresponding class is correct or not.
+ */
+
+public class InnerNode extends ParseNode {
+	@Override
+	public boolean check() {
+		// use super.check() to check the executor, then to check the distributor.
+		if ( super.check() ) {
+			try {
+				this.distributor = Class.forName(this.dis);
+			} catch (ClassNotFoundException e) {
+				return false;
+			}
+			try {
+				this.disMethod = this.distributor.getMethod("distribute", String.class);
+			} catch (SecurityException e) {
+				return false;
+			} catch (NoSuchMethodException e) {
+				return false;
+			}
+			if ( checkNext() ) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		return false;
+	}
+	
+	//	to check whether the distributor is correct.
+	//	to do.
+	private boolean checkNext() {
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
--- src/parser/core/ParseNode.java
+++ src/parser/core/ParseNode.java
@@ -10,48 +10,87 @@ import java.util.Vector;
  * 
  * InputNode, OutputNode, InnerNode are all son-classs of the ParseNode.
  * 
+ * check(), addNext() are two main class.
+ * 
  */
 public class ParseNode {
-	private String name;
-	private String exe;
-	private Class<?> executor;
-	private Vector<ParseNode> next;
+	// Node Name
+	protected String name;
+	
+	//	Executor Name
+	protected String exe;
+	
+	//	Executor class, by reflecting the String exe.
+	protected Class<?> executor;
+	
+	//	Distributor Name
+	protected String dis;
+	
+	//	Distributor Class
+	protected Class<?> distributor;
+	
+	//	The next ParseNode that this node is connected.
+	protected Vector<ParseNode> next;
+	
+	// process method
+	protected Method proMethod;
+	
+	// distribute method
+	protected Method disMethod;
 	
 	// Constructor of a ParseNode.
-	ParseNode() {
+	public ParseNode() {
 		this.name = null;
 		this.exe = null;
 		this.next = new Vector<ParseNode>();
 	}
 	
-	ParseNode( String name ) {
+	public ParseNode( String name ) {
 		this();
 		this.name = name;
 	}
 	
-	ParseNode( String name, String exe ) {
+	public ParseNode( String name, String exe ) {
 		this(name);
 		this.exe = exe;
+	}
+	
+	/*
+	 * Set the distribution class.
+	 * For the output node, the distribution class may not be applicable.
+	 */
+	public void setDis(String ss) {
+		this.dis = ss;
+		return;
 	}
 	
 	/*
 	 * ParseNode need to check whether the jar class can be find.
 	 * By check(), ParseNode reflects the String into a java class.
 	 */
-	boolean check() {
+	public boolean check() {
+		// First, check the executor
 		try {
 			this.executor = Class.forName(this.exe);
 		} catch (ClassNotFoundException e) {
 			return false;
 		}
 		try {
-			Method method = this.executor.getMethod("processor", String.class);
+			this.proMethod = this.executor.getMethod("processor", String.class);
 		} catch (SecurityException e) {
 			return false;
 		} catch (NoSuchMethodException e) {
 			return false;
 		}
 		return true;
+	}
+	
+	/*
+	 * Add a new ParseNode as one next node of the current node.
+	 */
+	public void addNext( ParseNode pn ) {
+		this.next.add(pn);
+		return;
 	}
 	
 	public static void main(String[] args) {
