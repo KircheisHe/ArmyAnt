24f41936a5a8d6a3fabdb0ccf5b5b3a4d6fc63b8
KircheisHe <hechenzhang@126.com>
2013-04-02 14:55:03 +0800

    2:55

--- src/parser/core/InputNode.java
+++ src/parser/core/InputNode.java
@@ -7,13 +7,22 @@ import java.io.File;
  * which get the information from a certain file or something else.
  * It is different from the InnerNode or the OutputNode.
  * InputNode basically will not have any executor or distributor.
+ * 
+ * InputNode should have only one output to a certain node.
  */
 public class InputNode extends ParseNode {
 	private String input;
 	private File file;
+	private ParseNode next;
 	
 	public InputNode(String inputFile) {
 		this.input = inputFile;
+		this.next = null;
+	}
+	
+	public boolean addNext(ParseNode pn) {
+		this.next = pn;
+		return true;
 	}
 	
 	/*
@@ -25,6 +34,7 @@ public class InputNode extends ParseNode {
 		if( !this.file.exists() ) {
 			return false;
 		}
+		if ( this.next == null ) return false;
 		return true;
 	}
 	
