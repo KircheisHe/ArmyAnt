b9964cee3423bf1729c4deca04fbf8feb956c4c1
KircheisHe <hechenzhang@126.com>
2013-04-02 15:08:09 +0800

    Finish with innerDag, with first edition.
    Needs to go with XML Parser and Engine.

---
+++ src/common/conf/Configuration.java
@@ -0,0 +1,12 @@
+package common.conf;
+
+public class Configuration {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/parser/xml/XmlParser.java
+++ src/parser/xml/XmlParser.java
@@ -1,12 +1,17 @@
 package parser.xml;
 
-public class XmlParser {
+import java.io.File;
 
+/*
+ * XmlParser reads the XML, and converts the XML into an InnerDAG.
+ * 
+ */
+public class XmlParser {
+	private File configurationFile;
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
